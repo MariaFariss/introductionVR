7fd3f9161a04b64d17351e7f4db873d40254963c
MariaFariss <maria.fariss.etu@univ-lemans.fr>
2022-12-12 18:41:45 +0100

    scenev2Audio

--- adamnatorVR/Assets/FireBlletOnValidate.cs
+++ adamnatorVR/Assets/FireBlletOnValidate.cs
@@ -8,15 +8,18 @@ public class FireBlletOnValidate : MonoBehaviour
     public GameObject bullet;
     public Transform SpawnPoint;
     public float fireSpeed = 20;
+    private AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(FireBullet);
     }
 
     public void FireBullet(ActivateEventArgs arg)
     {
+        audioSource.Play();
         GameObject spawnedBullet = Instantiate(bullet);
         spawnedBullet.transform.position = SpawnPoint.position;
         spawnedBullet.GetComponent<Rigidbody>().velocity = SpawnPoint.forward * fireSpeed;
